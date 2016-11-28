0ead7efe875d016fee4b8712c2ebc0e5f69b339d
Anirudha <anirudhamahale@gmail.com>
2016-11-28 11:21:13 +0530

    Initial Commit.

--- TryAutoLayout/AutoLayout.swift
+++ TryAutoLayout/AutoLayout.swift
@@ -11,24 +11,45 @@ import UIKit
 let screenSize = UIScreen.mainScreen().bounds // App Screen Size
 let storyboardSize: CGSize = CGSize(width: 375, height: 667)
 
-func resizeWidthDependingOnDevice(width: CGFloat) {
-    _ = screenSize.width * (width/storyboardSize.width)
+func resizeDependingOnDevice(view: UIView) {
+//    for constraint in view.constraints {
+//        switch constraint.identifier ?? "" {
+//        case "width":
+//            constraint.constant = getCalculatedWidth(constraint.constant)
+//        case "height":
+//            constraint.constant = getCalculatedHeight(constraint.constant)
+//        default:
+//            break
+//        }
+//    }
+//    
+//    for tempView in view.subviews {
+//        for constraint in tempView.constraints {
+//            switch constraint.identifier ?? "" {
+//            case "width":
+//                constraint.constant = getCalculatedWidth(constraint.constant)
+//            case "height":
+//                constraint.constant = getCalculatedHeight(constraint.constant)
+//            default:
+//                break
+//            }
+//        }
+//    }
+    
+    // A better way and performance
+    // updating view constraints
+    _ = view.constraints.filter{ $0.identifier == "height" }.map{ $0.constant = getCalculatedHeight($0.constant) }
+    _ = view.constraints.filter{ $0.identifier == "width" }.map{ $0.constant = getCalculatedWidth($0.constant) }
+    
+    // updating subviews constraints
+    _ = view.subviews.map{ $0.constraints.filter{ $0.identifier == "height" }.map{ $0.constant = getCalculatedHeight($0.constant) } }
+    _ = view.subviews.map{ $0.constraints.filter{ $0.identifier == "width" }.map{ $0.constant = getCalculatedWidth($0.constant)} }
 }
 
-func resizeHeightDependingOnDevice(height: CGFloat) {
-    _ = screenSize.height * (height/storyboardSize.height)
+func getCalculatedWidth(constant: CGFloat) -> CGFloat {
+    return screenSize.width * (constant/storyboardSize.width)
 }
 
-/*
- func adjustLayout(){
- var layout = [Any]()
- 
- layout.append(SEView(view: logoImageView, topSpace: logoImageViewTopSpace))
- layout.append(SEView(view: buyOneGetOneLabel, topSpace: buyOneGetOneLabelTopSpace))
- 
- setLayout(layout)
- }
- 
- 
- 
- */
+func getCalculatedHeight(constant: CGFloat) -> CGFloat {
+    return screenSize.height * (constant/storyboardSize.height)
+}
