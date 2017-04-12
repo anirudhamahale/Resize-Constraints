48483da9dce13530918a34d747984cb4140ce44d
Anirudha Mahale <anirudhamahale@gmail.com>
2017-04-12 12:15:02 +0530

    Updated Library to Swift 3 and added new features.

--- TryAutoLayout/AutoLayout.swift
+++ TryAutoLayout/AutoLayout.swift
@@ -8,34 +8,10 @@
 
 import UIKit
 
-let screenSize = UIScreen.mainScreen().bounds // App Screen Size
+let screenSize = UIScreen.main.bounds // App Screen Size
 let storyboardSize: CGSize = CGSize(width: 375, height: 667)
 
-func resizeDependingOnDevice(view: UIView) {
-//    for constraint in view.constraints {
-//        switch constraint.identifier ?? "" {
-//        case "width":
-//            constraint.constant = getCalculatedWidth(constraint.constant)
-//        case "height":
-//            constraint.constant = getCalculatedHeight(constraint.constant)
-//        default:
-//            break
-//        }
-//    }
-//    
-//    for tempView in view.subviews {
-//        for constraint in tempView.constraints {
-//            switch constraint.identifier ?? "" {
-//            case "width":
-//                constraint.constant = getCalculatedWidth(constraint.constant)
-//            case "height":
-//                constraint.constant = getCalculatedHeight(constraint.constant)
-//            default:
-//                break
-//            }
-//        }
-//    }
-    
+func resizeDependingOnDevice(_ view: UIView) {
     // A better way and performance
     // updating view constraints
     _ = view.constraints.filter{ $0.identifier == "height" }.map{ $0.constant = getCalculatedHeight($0.constant) }
@@ -44,12 +20,49 @@ func resizeDependingOnDevice(view: UIView) {
     // updating subviews constraints
     _ = view.subviews.map{ $0.constraints.filter{ $0.identifier == "height" }.map{ $0.constant = getCalculatedHeight($0.constant) } }
     _ = view.subviews.map{ $0.constraints.filter{ $0.identifier == "width" }.map{ $0.constant = getCalculatedWidth($0.constant)} }
+    
+    let emptyPoint = CGFloat(0.0)
+    func configureLabel(view: UIView) {
+        let label = view as! UILabel
+        label.font = label.font.withSize(getCalculatedHeight(label.font.pointSize))
+    }
+    
+    func configureTextField(view: UIView) {
+        let textField = view as! UITextField
+        textField.font = textField.font?.withSize(getCalculatedHeight((textField.font?.pointSize) ?? emptyPoint))
+    }
+    
+    func configureButton(view: UIView) {
+        let button = view as! UIButton
+        button.titleLabel?.font = button.titleLabel?.font.withSize(getCalculatedHeight((button.titleLabel?.font.pointSize) ?? emptyPoint))
+    }
+    
+    func configureTextView(view: UIView) {
+        let textView = view as! UITextView
+        textView.font = textView.font?.withSize(getCalculatedHeight((textView.font?.pointSize) ?? emptyPoint))
+    }
+        
+    for item in view.subviews {
+        switch item {
+        case is UILabel:
+            configureLabel(view: item)
+        case is UITextField:
+            configureTextField(view: item)
+        case is UIButton:
+            configureButton(view: item)
+        case is UITextView:
+            configureTextView(view: item)
+        default:
+            break
+        }
+    }
+    
 }
 
-func getCalculatedWidth(constant: CGFloat) -> CGFloat {
+func getCalculatedWidth(_ constant: CGFloat) -> CGFloat {
     return screenSize.width * (constant/storyboardSize.width)
 }
 
-func getCalculatedHeight(constant: CGFloat) -> CGFloat {
+func getCalculatedHeight(_ constant: CGFloat) -> CGFloat {
     return screenSize.height * (constant/storyboardSize.height)
 }
